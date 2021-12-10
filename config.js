5f7d9b6a39e4bb6dbc78b1eaec9882a075a3f9bd
TunZiet <tuanvietvungoc123@gmail.com>
2021-12-10 21:46:45 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Dong se ooo, hello a di no mo to ',
+    introDesc: `Tớ có điều này muốn hỏi cậu nè, trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Làm ngừi iu tớ nhaaa hiii 😙 ',
+    desc: 'Trả lời đi đừng có mà thoát ra hay k trả lời ',
+    btnYes: 'Đồng ý hihihihi 💖',
+    btnNo: 'Không (không chọn đc đâu đừng có mà cố 🙂) ',
+    question:'Sao lại đồng ý đóooo',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Send',
+    reply: 'Tại vì m đẹp trai đó hi',
+    mess: 't biết mà hehe',
+    messDesc: 'xong rồi thì ib cho t rồi cho t biết là cta thành đôi r nha :))',
+    btnAccept: 'Vânggg',
+    messLink: 'https://www.facebook.com/tunziet/'
+}
